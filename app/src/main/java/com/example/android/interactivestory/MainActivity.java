f18b753d9cc8355844e8190842eefc2ae4d3e896
charlee593 <charlee593@gmail.com>
2015-01-17 04:04:19 -0500

    Fix startActivity in MainActivity

--- app/src/main/java/com/example/android/interactivestory/MainActivity.java
+++ app/src/main/java/com/example/android/interactivestory/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends ActionBarActivity {
 
     private void startStory(String name){
         Intent intent = new Intent(this, StoryActivity.class);
-        startActivity(intent);
         intent.putExtra(getString(R.string.key_name), name);
+        startActivity(intent);
     }
 }
